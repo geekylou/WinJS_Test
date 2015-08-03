6df06ce418c8f116295bbf5dcb8b45ff3d0ccc9c
Louise Newberry <nn6215@googlemail.com>
2015-08-03 21:41:56 +0100

    Initial version

---
+++ App1/js/default.js
@@ -0,0 +1,80 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+	"use strict";
+
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
+
+	app.onactivated = function (args) {
+		if (args.detail.kind === activation.ActivationKind.launch) {
+			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+				// TODO: This application has been newly launched. Initialize your application here.
+			} else {
+				// TODO: This application was suspended and then terminated.
+				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+			}
+			args.setPromise(WinJS.UI.processAll());
+		}
+	};
+
+	app.oncheckpoint = function (args) {
+		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+	};
+
+	app.start();
+})();
+
+WinJS.Namespace.define("Account", {
+    signInCommand: WinJS.UI.eventHandler(function (ev) {
+        var status = document.querySelector(".status");
+        var command = ev.currentTarget;
+        if (command.winControl) {
+            var label = command.winControl.label || command.winControl.icon || "button";
+            var section = command.winControl.section || "";
+            var msg = section + " command " + label + " was pressed";
+            document.querySelector("#login-dialog").winControl.show();
+
+
+            var contentDialog = document.querySelector(".win-contentdialog").winControl;
+            contentDialog.addEventListener("beforehide", Account.quitCommand, false);
+        }
+    }),
+
+    quitCommand: WinJS.UI.eventHandler(function (ev) {
+        var command = ev.currentTarget;
+        if (ev.detail.result == "primary") {
+            var app = WinJS.Application;
+            close();
+        }
+        else
+        {
+            
+        }
+    })
+
+});
+
+WinJS.Namespace.define("Sample", {
+    outputCommand: WinJS.UI.eventHandler(function (ev) {
+        var status = document.querySelector(".status");
+        var command = ev.currentTarget;
+        if (command.winControl) {
+            var label = command.winControl.label || command.winControl.icon || "button";
+            var section = command.winControl.section || "";
+            var msg = section + " command " + label + " was pressed";
+
+            var notifications = Windows.UI.Notifications;
+            var template = notifications.ToastTemplateType.toastImageAndText01;
+            var toastXml = notifications.ToastNotificationManager.getTemplateContent(template);
+
+            var toastTextElements = toastXml.getElementsByTagName("text");
+            toastTextElements[0].appendChild(toastXml.createTextNode("Hello World!"));
+            var toast = new notifications.ToastNotification(toastXml);
+            var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
+            toastNotifier.show(toast);
+        }
+    })
+});
